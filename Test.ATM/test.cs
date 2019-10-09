4f4623ae4e163b978d595e2f1446fd1563567b0a
martinsekd <201710685@post.au.dk>
2019-10-09 13:21:40 +0200

    new test methods

---
+++ Test.ATM/test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.ATM
+{
+    class test
+    {
+        public void metodeA()
+        {
+            int a = 0;
+            a++;
+        }
+
+        public void metodeB()
+        {
+            int a = 0;
+            if(a==1)
+            {
+                Console.WriteLine("tal er 1");
+            } else
+            {
+                Console.WriteLine("Tal er 0");
+            }
+        }
+    }
+}
