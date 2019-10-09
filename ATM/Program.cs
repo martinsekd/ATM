badbd8e152c35aaedb529ce71ca0e8618b9e53a5
martinsekd <201710685@post.au.dk>
2019-10-09 14:21:49 +0200

    new receiver

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace ATM
 {
-    class Program
+    public class Program
     {
+        
         static void Main(string[] args)
         {
+            var rec = TransponderReceiver.TransponderReceiverFactory.CreateTransponderDataReceiver();
+
+            rec.TransponderDataReady += receive;
+
+            while(true)
+            {
+
+            }
+            
+        }
+
+        static public void receive(object sender, TransponderReceiver.RawTransponderDataEventArgs e)
+        {
+            foreach (var data in e.TransponderData)
+            {
+                System.Console.WriteLine($"Transponderdata {data}");
+            }
         }
     }
 }
