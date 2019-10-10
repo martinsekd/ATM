1eafdf9897cb17b81c312fa0918a051b3e1fa1dc
martinsekd <201710685@post.au.dk>
2019-10-10 16:19:46 +0200

    added a Seperation function

--- ATM/FlightCollection.cs
+++ ATM/FlightCollection.cs
@@ -8,5 +8,23 @@ namespace ATM
 {
     class FlightCollection
     {
+        private bool seperation = false;
+        public void Seperation(TransponderData t1, TransponderData t2)
+        {
+            int deltaX = t1.X - t2.X;
+            int deltaY = t1.Y - t2.Y;
+
+            double distance = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+            int altitudeDistance = Math.Abs(t1.Altitude - t2.Altitude);
+
+            if(altitudeDistance < 300 && distance < 5000)
+            {
+                seperation = true;
+            } else
+            {
+                seperation = false;
+            }
+
+        }
     }
 }
