7e2089431d501ad945b98774c52f9e6566f99caf
martinsekd <201710685@post.au.dk>
2019-10-10 15:57:44 +0200

    added calculate degree and speed

--- ATM/FlightCalculator.cs
+++ ATM/FlightCalculator.cs
@@ -10,7 +10,47 @@ namespace ATM
     {
         public int CalculateSpeed(TransponderData oldData, TransponderData newData)
         {
-            return 0;
+            int deltaX = oldData.X - newData.X;
+            int deltaY = oldData.Y - newData.Y;
+
+            double distance = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+            TimeSpan timespan = newData.Time - oldData.Time;
+            int timedif = (int)timespan.TotalMilliseconds;
+            return (int) Math.Round(distance/(timedif*1000),0);
         }
+
+        public double CalculateDegree(TransponderData oldData, TransponderData newData)
+        {
+            double deltaX = newData.X - oldData.X;
+            double deltaY = newData.Y - oldData.Y;
+
+            double toDegreeFactor = 360 / (2 * Math.PI);
+
+            double angle = Math.Atan(deltaY/deltaX)*toDegreeFactor;
+
+            double degree = 0;
+            if(newData.X > oldData.X)
+            {
+                degree = 90 - angle;
+            } else if (newData.X < oldData.X)
+            {
+                degree = 360 - (angle + 90);
+            } else
+            {
+                if(newData.Y > oldData.Y)
+                {
+                    degree = 0;
+                } else if(newData.Y < oldData.Y)
+                {
+                    degree = 180;
+                } else
+                {
+                    degree = 0;
+                }
+            }
+
+            return degree;
+        }
+
     }
 }
--- ATM/Program.cs
+++ ATM/Program.cs
@@ -15,7 +15,12 @@ namespace ATM
 
             rec.TransponderDataReady += receive;
 
-            while(true)
+            FlightCalculator fc = new FlightCalculator();
+            TransponderData t1 = new TransponderData("HEJA",0, 0, 20, new DateTime());
+            TransponderData t2 = new TransponderData("HEJA", -9, -4, 20, new DateTime());
+
+            Console.WriteLine(fc.CalculateDegree(t1,t2));
+            while (true)
             {
 
             }
@@ -27,8 +32,8 @@ namespace ATM
             List<TransponderData> TransponderListe = new List<TransponderData>();
             foreach (var data in e.TransponderData)
             {
-                string[] newstring = data.Split(';');
-                TransponderListe.Add(new TransponderData(newstring[0], newstring[1], newstring[2], newstring[3], newstring[4]));
+                //string[] newstring = data.Split(';');
+                //TransponderListe.Add(new TransponderData(newstring[0], newstring[1], newstring[2], newstring[3], newstring[4]));
                 //System.Console.WriteLine($"Transponderdata {data}");
             }
         }
