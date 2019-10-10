5f2618f2c867f876300b648663cb4e3055f0d994
legbeard <au484146@uni.au.dk>
2019-10-10 14:04:29 +0200

    Stubbed the FlightCalculator to fix Git being weird

--- ATM/FlightCalculator.cs
+++ ATM/FlightCalculator.cs
@@ -10,7 +10,7 @@ namespace ATM
     {
         public int CalculateSpeed(TransponderData oldData, TransponderData newData)
         {
-            return oldData.
+            return 0;
         }
     }
 }
