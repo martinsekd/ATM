245e057de78a6af83975da4d29034272bb49d1e4
martinsekd <201710685@post.au.dk>
2019-10-13 23:33:09 +0200

    add new tests

--- Test.ATM/TestATM.cs
+++ Test.ATM/TestATM.cs
@@ -42,6 +42,12 @@ namespace Test.ATM
         }
 
         [TestCase(10,40,70,-100,156.80)]
+        [TestCase(10, 40, 10, 80, 180)]
+        [TestCase(10, 40, 10, 10, 0)]
+        [TestCase(10, 40, 10, 40, 0)]
+        [TestCase(10, 100, 11, 10, 179.36)]
+        [TestCase(10, 100, 11, 200, 0.57)]
+        [TestCase(0, 0, -1, -100, 180.57)]
         public void CalculateDirection_add2TransponderData_c (int x1, int y1, int x2, int y2, double c)
         {
             TransponderData t1 = new TransponderData("TEST", x1, y1, 100, new DateTime());
