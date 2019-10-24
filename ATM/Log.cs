8e70ce6917a2cfb44fa93d3c97250878dd69ade7
Marcus Bech <43644925+morogfar21@users.noreply.github.com>
2019-10-24 12:59:29 +0200

    Log

--- ATM/Log.cs
+++ ATM/Log.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,24 @@ namespace ATM
 {
     class Log : ILog
     {
+        public static bool WriteLog(string strFileName, string strMessage)
+        {
+            try
+            {
+                FileStream objFilestream = new FileStream(string.Format("{0}\\{1}", Path.GetTempPath(), strFileName), FileMode.Append, FileAccess.Write);
+                StreamWriter objStreamWriter = new StreamWriter((Stream)objFilestream);
+                objStreamWriter.WriteLine(strMessage);
+                objStreamWriter.Close();
+                objFilestream.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
 
+        WriteLogFile.WriteLog("ConsoleLog.txt", 
+        String.Format("{0} @ {1}", "Log is Created by Martinique and Magnus the mosquito", DateTime.Now));
     }
 }
