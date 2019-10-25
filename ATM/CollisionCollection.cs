2dd6060936044e3bb447d125c8df65adf3d6df97
martinsekd <201710685@post.au.dk>
2019-10-25 19:31:15 +0200

    edited a bit in collisionCollection

--- ATM/CollisionCollection.cs
+++ ATM/CollisionCollection.cs
@@ -4,12 +4,21 @@ using NUnit.Framework.Constraints;
 
 namespace ATM.System
 {
+    public enum koorTypes
+    {
+        X,
+        Y,
+        H
+    }
+
     public class CollisionCollection: ICollisionCollection
     {
+        
         private List<Collision> collisions = new List<Collision>();
-
+        
         public CollisionCollection()
         {
+            
         }
 
         public CollisionCollection(List<Collision> collisions)
@@ -27,6 +36,81 @@ namespace ATM.System
 
             return newCollisions;
 
+        }
+        private void startQuicksort(List<Flight> ar, int n,koorTypes type)
+        {
+            quicksort(ar, 0, n - 1, type);
+        }
+
+        private void quicksort(List<Flight> ar, int start, int slut, koorTypes type)
+        {
+            if (slut - start < 1) return;
+            Flight pivot = ar[slut];
+            int i = start - 1;
+            int j = start;
+
+            while (j < slut)
+            {
+                //if (ar[j] > pivot)
+                if (compare(pivot, ar[j], type))
+                {
+                    j++;
+                }
+                else
+                {
+                    i++;
+                    Flight a = ar[i];
+                    Flight b = ar[j];
+                    ar[i] = b;
+                    ar[j] = a;
+                    j++;
+                }
+            }
+            Flight b2 = ar[i + 1];
+            ar[i + 1] = pivot;
+            ar[slut] = b2;
+
+            quicksort(ar, start, i, type);
+            quicksort(ar, i + 2, slut, type);
+        }
+
+        private bool compare(Flight a,Flight b, koorTypes type)
+        {
+            switch(type)
+            {
+                case koorTypes.X:
+                    if(a.TData.X<b.TData.X)
+                    {
+                        return true;
+                    } else
+                    {
+                        return false;
+                    }
+                    break;
+                case koorTypes.Y:
+                    if (a.TData.Y < b.TData.Y)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                    break;
+                case koorTypes.H:
+                    if (a.TData.Altitude < b.TData.Altitude)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                    break;
+                default:
+                    return true;
+                    break;
+            }
         }
     }
 }
